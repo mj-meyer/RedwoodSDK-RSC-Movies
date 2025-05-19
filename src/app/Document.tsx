18a1029b05c6a3b897ed31810a565f60c94efe27
MJ Meyer <hello@mjmeyer.dev>
2025-05-19 22:41:27 +1200

    feat: add css and tailwind

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles.css?url";
+
 export const Document: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
@@ -6,9 +8,16 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-standard</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Boldonse&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Instrument+Serif&display=swap"
+        rel="stylesheet"
+      />
       <link rel="modulepreload" href="/src/client.tsx" />
+      <link rel="stylesheet" href={styles} />
     </head>
-    <body>
+    <body className="font-instrumentSans pb-56">
       <div id="root">{children}</div>
       <script>import("/src/client.tsx")</script>
     </body>
